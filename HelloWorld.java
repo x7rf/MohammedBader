5d5e9e99d428d2298d8b68fff23e25ff4e64dae4
MohammedBader <moayd5028@gmail.com>
2026-09-14 14:05:20 +0300

    Update HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,6 @@
-//Mohammed Bader.
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        System.out.println("This is my first Git project.");
+    }
+}
