9610eeb20c1bdc00f62d73bbf9075b77f9af2098
MohammedBader <moayd5028@gmail.com>
2026-09-14 14:11:51 +0300

    Hello Dr.Mawal

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
+        System.out.println("Hello, GitHub! Hello Dr.mawal");
         System.out.println("This is my first Git project.");
     }
 }
